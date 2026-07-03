cafc53e5fbd6a964a28d10a209a2b8b44df6bc09
Eric Chen <eric.chen682@gmail.com>
2026-07-02 23:06:39 -0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto val : stones)
+            pq.push(val);
+        while(pq.size() > 1)
+        {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x == y)
+                continue;
+            pq.push(abs(x-y));
+        }
+        if(pq.size() == 0)
+            return 0;
+        return pq.top();
+    }
+};
